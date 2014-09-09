e2b1a3450a3d5e9a2dd7cebc71a10d3267655210
theRealGabacho <bburket@live.com>
2014-09-09 02:14:40 -0700

    Style updates and angular integration

--- public/scripts/homeController.js
+++ public/scripts/homeController.js
@@ -1,3 +1,15 @@
-﻿sweetSuite.controller('homeController', ['$scope', function($scope) {
-    $scope.testText = "This is some test text";
+﻿sweetSuite.controller('homeController', ['$scope', function ($scope) {
+    
+    $scope.plugins = [
+        { name: "Home", templateUrl: "" },
+        { name: "GuidGen", templateUrl: "" },
+        { name: "Pretty Json", templateUrl: "" }
+    ];
+
+
+    $scope.selectedPlugin = $scope.plugins[0];
+
+    $scope.pluginNavClick = function(plugin) {
+        $scope.selectedPlugin = plugin;
+    }
 }]);
